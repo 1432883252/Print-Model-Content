c36ea9197cef68ee4defed8a9dc21a5771a79477
1432883252 <1432883252@qq.com>
2016-07-28 15:40:08 +0800

    直接打印Model
    直接打印Model里的内容，而不是地址

---
+++ NSObject+DebugDescription.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+DebugDescription.h
+//  test_button
+//
+//  Created by wangpanpan on 16/7/1.
+//  Copyright © 2016年 wangpanpan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (DebugDescription)
+
+@end
